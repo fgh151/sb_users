a3372afd562638a1df0ddd2b1d2e36f8ed49dda4
fgorsky <fedor@support-pc.org>
2023-11-18 11:36:26 +0300

    init

---
+++ src/main/java/ru/skillbox/demo/service/UserService.java
@@ -0,0 +1,55 @@
+package ru.skillbox.demo.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import ru.skillbox.demo.entity.User;
+import ru.skillbox.demo.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String createUser(User user) {
+        User saved = userRepository.save(user);
+
+        return saved.toString();
+    }
+
+    public User getUser(long id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public String updateUser(User user, long id) {
+        if (!userRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        userRepository.save(user);
+
+        return "Success";
+    }
+
+    public String deleteUser(long id) {
+
+        if (!userRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        userRepository.deleteById(id);
+
+
+        return "Success";
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
